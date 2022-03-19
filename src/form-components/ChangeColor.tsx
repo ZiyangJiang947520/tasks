810f400cb36330b10972056c5bb611fd7a48a2ee
zyjiang@udel.edu <zyjiang@udel.edu>
2022-03-18 21:25:53 -0400

    task10 finished

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,49 @@
 import React, { useState } from "react";
-const [answer, setAnswer] = useState<string>("");
+import { Form } from "react-bootstrap";
+
+const COLORS = [
+    "white",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "purple",
+    "black"
+];
+
 export function ChangeColor(): JSX.Element {
+    const [chosen, setChosen] = useState<string>("white");
     return (
         <div>
             <h3>Change Color</h3>
+            {COLORS.map((color: string) => (
+                <Form.Check
+                    type="radio"
+                    name="colors"
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                        setChosen(event.target.value)
+                    }
+                    key={color}
+                    inline
+                    id={"colors-choice" + color}
+                    label={
+                        <span style={{ backgroundColor: color }}>{color}</span>
+                    }
+                    value={color}
+                    checked={chosen === color}
+                />
+            ))}
+            <p>
+                You have chosen:{" "}
+                <span
+                    data-testid="colored-box"
+                    style={{ backgroundColor: chosen }}
+                >
+                    {chosen}
+                </span>
+            </p>
         </div>
     );
 }
